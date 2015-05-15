fabceb1d762fb3c810979d5df18094f5d6e7baff
Zhou Li <zhoulijoe@gmail.com>
2015-05-15 02:04:46 -0700

    Add more test for class bascis

--- ObjcBasicTests/class/ZLIClassTest.m
+++ ObjcBasicTests/class/ZLIClassTest.m
@@ -4,7 +4,10 @@
 // Interface usually goes in .h file
 @interface ZLIClassTestBasic : NSObject
 
-// A public property
+/*
+ * A public property, an instance variable with a '_' prefix as the name will be synthesized by compiler by default.
+ * The default attributes are: strong, atomic, readwrite.
+ */
 @property NSString *strProperty;
 
 // Readonly property
@@ -16,10 +19,98 @@
 // Multple attributes are separated by comma
 @property (strong, nonatomic, readonly) NSString *multiAttrProperty;
 
+// Property with custom getter and setter, needs to be nonatomic, needs to explicitly synthesize instance variable in this case
+@property (nonatomic) NSString *customAccessorProperty;
+
+// Readonly property without instance variable
+@property (readonly) NSString *noIvarProperty;
+
+// Property with initial value
+@property NSString *propertyWithInitialValue;
+
+// Lazily initialized property
+@property (nonatomic) NSString *lazyProperty;
+
+// Counters for test purpose
+@property NSUInteger getterCalled;
+@property NSUInteger setterCalled;
+
 @end
 
 // Implementation usually goes in .m file
+@interface ZLIClassTestBasic ()
+
+// Dispatch queue for thread safe lazy-loading getter
+@property dispatch_queue_t syncQueue;
+
+@end
+
 @implementation ZLIClassTestBasic
+
+- (instancetype)init {
+    self = [super init];
+
+    if (self) {
+        // Use instance variable in initializer
+        _syncQueue = dispatch_queue_create("ZLIClassTestBasicSyncQueue", 0);
+        _propertyWithInitialValue = @"hello";
+    }
+
+    return self;
+}
+
+/*
+ * Compiler only synthesize instance variable when as least one accessor is being synthesized, you'll need to
+ * specifically request an instance variable to be synthesized if getter(for readonly) or getter + setter have been
+ * overriden.
+ */
+@synthesize customAccessorProperty = _customAccessorProperty;
+
+/**
+ * Custom getter for customAccessorProperty
+ *
+ * @return current value of property
+ */
+- (NSString *)customAccessorProperty {
+    self.getterCalled ++;
+
+    return _customAccessorProperty;
+}
+
+/**
+ * Custom setter for customAccessorProperty
+ *
+ * @param customAccessorProperty New value to set the property to
+ */
+- (void)setCustomAccessorProperty:(NSString *)customAccessorProperty {
+    self.setterCalled ++;
+
+    _customAccessorProperty = customAccessorProperty;
+}
+
+/**
+ * Overridding getter for readonly property prevents instance variable from being created
+ *
+ * @return Property value
+ */
+- (NSString *)noIvarProperty {
+    return @"hello";
+}
+
+/**
+ * Thread safe getter for lazy-loading property
+ *
+ * @return Property value
+ */
+- (NSString *)lazyProperty {
+    dispatch_sync(self.syncQueue, ^{
+        if (!_lazyProperty) {
+            _lazyProperty = @"hello";
+        }
+    });
+
+    return _lazyProperty;
+}
 
 @end
 
@@ -59,6 +150,41 @@ describe(@"class", ^{
             classTestBasic.booleanProperty = YES;
 
             [[theValue(classTestBasic.isBooleanProperty) should] beYes];
+        });
+
+        it(@"getter and setter for @property are synthesized automatically by compiler", ^{
+            ZLIClassTestBasic *classTestBasic = [ZLIClassTestBasic new];
+            [classTestBasic setStrProperty:@"hello"];
+
+            [[[classTestBasic strProperty] should] equal:@"hello"];
+        });
+
+        it(@"dot notation is shorthand for calling getter and setter", ^{
+            ZLIClassTestBasic *classTestBasic = [ZLIClassTestBasic new];
+            classTestBasic.customAccessorProperty = @"hello";
+            NSString *propertyValue = classTestBasic.customAccessorProperty;
+
+            [[theValue(classTestBasic.setterCalled) should] equal:theValue(1)];
+            [[theValue(classTestBasic.getterCalled) should] equal:theValue(1)];
+            [[propertyValue should] equal:@"hello"];
+        });
+
+        it(@"can exist without backing instance variable", ^{
+            ZLIClassTestBasic *classTestBasic = [ZLIClassTestBasic new];
+
+            [[classTestBasic.noIvarProperty should] equal:@"hello"];
+        });
+
+        it(@"can be initialized at creation time", ^{
+            ZLIClassTestBasic *classTestBasic = [ZLIClassTestBasic new];
+
+            [[classTestBasic.propertyWithInitialValue should] equal:@"hello"];
+        });
+
+        it(@"can be lazily initialized when it's first used", ^{
+            ZLIClassTestBasic *classTestBasic = [ZLIClassTestBasic new];
+
+            [[classTestBasic.lazyProperty should] equal:@"hello"];
         });
     });
 });
