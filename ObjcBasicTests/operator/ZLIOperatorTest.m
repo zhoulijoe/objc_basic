03e320b30491c086c236b0e7f5f1a48e2d3488e5
Zhou Li <zhoulijoe@gmail.com>
2015-06-09 09:46:38 -0700

    Add test for ++ operator

--- ObjcBasicTests/operator/ZLIOperatorTest.m
+++ ObjcBasicTests/operator/ZLIOperatorTest.m
@@ -3,6 +3,30 @@
 SPEC_BEGIN(Operator)
 
 describe(@"Basic operator syntax", ^{
+    context(@"assignment", ^{
+        it(@"single increment assignment using ++", ^{
+            NSInteger count = 0;
+            count++;
+            ++count;
+
+            [[theValue(count) should] equal:theValue(2)];
+        });
+
+        it(@"prefix using ++ increment then evaluate variable", ^{
+            NSInteger count = 0;
+            NSInteger value = ++count;
+
+            [[theValue(value) should] equal:theValue(1)];
+        });
+
+        it(@"suffix using ++ evaluate then increment variable", ^{
+            NSInteger count = 0;
+            NSInteger value = count++;
+
+            [[theValue(value) should] equal:theValue(0)];
+        });
+    });
+
     context(@"math operator", ^{
         it(@"addition/subtraction", ^{
             [[theValue(1 + 1) should] equal:theValue(2)];
